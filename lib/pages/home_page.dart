3b4a75c41e108e0c3ca269e75551dfa8db3626e6
L Palharês Rosa <lucianopalharez@gmail.com>
2024-06-22 16:11:27 -0300

    slider app bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food/components/my_drawer.dart';
+import 'package:flutter_food/components/my_sliver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,12 +12,28 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('home page'),
-        backgroundColor: Theme.of(context).colorScheme.surface,
-      ), 
+    return Scaffold( 
       drawer: const MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySliverAppBar(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Divider(
+                  indent: 25,
+                  endIndent: 25,
+                  color: Theme.of(context).colorScheme.secondary,
+                )
+              ],
+            ), 
+            title: Text('title')
+          )
+        ],
+        body: Container(
+          color: Colors.blue,
+        ),
+      )
     );
   }
 }
