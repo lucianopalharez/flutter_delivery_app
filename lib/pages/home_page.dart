36689cc9f9d8635021acef75fc4361051f81528b
L Palharês Rosa <lucianopalharez@gmail.com>
2024-06-22 16:49:23 -0300

    tab bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_food/components/my_current_location.dart';
 import 'package:flutter_food/components/my_description_box.dart';
 import 'package:flutter_food/components/my_drawer.dart';
 import 'package:flutter_food/components/my_sliver_app_bar.dart';
+import 'package:flutter_food/components/my_tab_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +12,25 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _tabController.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold( 
@@ -19,7 +38,7 @@ class _HomePageState extends State<HomePage> {
       body: NestedScrollView(
         headerSliverBuilder: (context, innerBoxIsScrolled) => [
           MySliverAppBar(
-            title: Text('title'),
+            title: MyTabBar(tabController: _tabController),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
@@ -39,9 +58,14 @@ class _HomePageState extends State<HomePage> {
             
           )
         ],
-        body: Container(
-          color: Colors.blue,
-        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            Text('Hello'),
+            Text('Flutter'),
+            Text('code')
+          ]
+        )
       )
     );
   }
