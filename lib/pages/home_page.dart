ea65a5592983815fc193123703a71c3725539e29
L Palharês Rosa <lucianopalharez@gmail.com>
2024-06-22 16:38:52 -0300

    slider app bar 2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food/components/my_current_location.dart';
+import 'package:flutter_food/components/my_description_box.dart';
 import 'package:flutter_food/components/my_drawer.dart';
 import 'package:flutter_food/components/my_sliver_app_bar.dart';
 
@@ -13,10 +15,11 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold( 
-      drawer: const MyDrawer(),
+      drawer: MyDrawer(),
       body: NestedScrollView(
         headerSliverBuilder: (context, innerBoxIsScrolled) => [
           MySliverAppBar(
+            title: Text('title'),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
@@ -24,10 +27,16 @@ class _HomePageState extends State<HomePage> {
                   indent: 25,
                   endIndent: 25,
                   color: Theme.of(context).colorScheme.secondary,
-                )
+                ),
+
+                const MyCurrentLocation(),
+
+                const MyDescriptionBox(),
+
+
               ],
             ), 
-            title: Text('title')
+            
           )
         ],
         body: Container(
