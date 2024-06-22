9f45cc7330bf28762047b7c03605c93b429a352e
L Palharês Rosa <lucianopalharez@gmail.com>
2024-06-22 18:27:57 -0300

    product page 2 - separar por columns diferentes

--- lib/pages/food_page.dart
+++ lib/pages/food_page.dart
@@ -19,26 +19,31 @@ class _FoodPageState extends State<FoodPage> {
         children: [
           Image.asset(widget.food.imagePath),
 
-          Text(widget.food.name),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [             
+              Text(widget.food.name),
 
-          Text(widget.food.description),
+              Text(widget.food.description),
 
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: widget.food.availableAddons.length,
-            physics: const NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              Addon addon = widget.food.availableAddons[index];
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: widget.food.availableAddons.length,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  Addon addon = widget.food.availableAddons[index];
 
-              return CheckboxListTile(
-                title: Text(addon.name),
-                subtitle: Text('R\$' + addon.price.toString()),
-                value: false, 
-                onChanged: (value) {
+                  return CheckboxListTile(
+                    title: Text(addon.name),
+                    subtitle: Text('R\$' + addon.price.toString()),
+                    value: false, 
+                    onChanged: (value) {
 
-                }
-              );
-            },
+                    }
+                  );
+                },
+              )
+            ],
           )
         ],
       ),
