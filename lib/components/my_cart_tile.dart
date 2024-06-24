91123347cd200c150f26f227a66c95fd63737494
L Palharês Rosa <lucianopalharez@gmail.com>
2024-06-24 08:54:09 -0300

    cart page 4 - my quantity selector

--- lib/components/my_cart_tile.dart
+++ lib/components/my_cart_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food/components/my_quantity_selector.dart';
 import 'package:flutter_food/models/cart_item.dart';
+import 'package:flutter_food/models/restaurant.dart';
 import 'package:provider/provider.dart';
 
 // ignore: must_be_immutable
@@ -10,13 +12,24 @@ class MyCartTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, value, child) => Container(
+    return Consumer<Restaurant>(
+      builder: (context, restaurant, child) => Container(
         child: Column(
           children: [
             Row(
               children: [
-                Image.asset(cartItem.food.imagePath),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    cartItem.food.imagePath,
+                    height: 100,
+                    width: 100,
+                  )
+                ),
+
+                SizedBox(
+                  height: 10,
+                ),
 
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -25,6 +38,17 @@ class MyCartTile extends StatelessWidget {
 
                     Text('R\$ ' + cartItem.food.price.toString())
                   ],
+                ),
+
+                MyQuantitySelector(
+                  quantity: cartItem.quantity, 
+                  food: cartItem.food, 
+                  onIncrement: () {
+                    restaurant.addToCart(cartItem.food, cartItem.selectedAddons);                    
+                  }, 
+                  onDecrement: () {
+                    restaurant.removeFromCart(cartItem);
+                  }, 
                 )
               ],
             )
