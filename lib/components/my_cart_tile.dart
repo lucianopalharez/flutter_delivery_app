fad1c4472a02847defb790ffa3258431883ac921
L Palharês Rosa <lucianopalharez@gmail.com>
2024-06-24 08:56:43 -0300

    cart page 5 - estilo container

--- lib/components/my_cart_tile.dart
+++ lib/components/my_cart_tile.dart
@@ -14,43 +14,52 @@ class MyCartTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<Restaurant>(
       builder: (context, restaurant, child) => Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary, 
+          borderRadius: BorderRadius.circular(8)
+        ),
+        margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
         child: Column(
           children: [
-            Row(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Image.asset(
-                    cartItem.food.imagePath,
-                    height: 100,
-                    width: 100,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset(
+                      cartItem.food.imagePath,
+                      height: 100,
+                      width: 100,
+                    )
+                  ),
+              
+                  SizedBox(
+                    height: 10,
+                  ),
+              
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(cartItem.food.name),
+              
+                      Text('R\$ ' + cartItem.food.price.toString())
+                    ],
+                  ),
+                  Spacer(),
+              
+                  MyQuantitySelector(
+                    quantity: cartItem.quantity, 
+                    food: cartItem.food, 
+                    onIncrement: () {
+                      restaurant.addToCart(cartItem.food, cartItem.selectedAddons);                    
+                    }, 
+                    onDecrement: () {
+                      restaurant.removeFromCart(cartItem);
+                    }, 
                   )
-                ),
-
-                SizedBox(
-                  height: 10,
-                ),
-
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(cartItem.food.name),
-
-                    Text('R\$ ' + cartItem.food.price.toString())
-                  ],
-                ),
-
-                MyQuantitySelector(
-                  quantity: cartItem.quantity, 
-                  food: cartItem.food, 
-                  onIncrement: () {
-                    restaurant.addToCart(cartItem.food, cartItem.selectedAddons);                    
-                  }, 
-                  onDecrement: () {
-                    restaurant.removeFromCart(cartItem);
-                  }, 
-                )
-              ],
+                ],
+              ),
             )
           ],
         ),
