d6e2c66bc11f220421e8850645bf20f7a7da6711
L Palharês Rosa <lucianopalharez@gmail.com>
2024-06-25 08:51:04 -0300

    progress page 2 - my receipt

---
+++ lib/components/my_receipt.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyReceipt extends StatelessWidget {
+  const MyReceipt({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25, right: 25, bottom: 25),
+      child: Column(
+        children: [
+          const Text('Thank you for your order!'), 
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Theme.of(context).colorScheme.secondary),
+              borderRadius: BorderRadius.circular(8)
+            ),
+            padding: const EdgeInsets.all(25),
+            child: const Text('Receipt here..'),
+          )
+        ],
+      ),
+    );
+  }
+}
