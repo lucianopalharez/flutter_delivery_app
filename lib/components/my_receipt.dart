e95d8fa93137e0cec680e9bd94266046eb9eb636
L Palharês Rosa <lucianopalharez@gmail.com>
2024-07-05 09:53:50 -0300

    progress page 4 - my receipt resume items'

--- lib/components/my_receipt.dart
+++ lib/components/my_receipt.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food/models/restaurant.dart';
+import 'package:provider/provider.dart';
 
 class MyReceipt extends StatelessWidget {
   const MyReceipt({super.key});
@@ -7,19 +9,30 @@ class MyReceipt extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 25, right: 25, bottom: 25, top: 50),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('Thank you for your order!'), 
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Theme.of(context).colorScheme.secondary),
-              borderRadius: BorderRadius.circular(8)
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Thank you for your order!'), 
+            const SizedBox(
+              height: 25,
             ),
-            padding: const EdgeInsets.all(25),
-            child: const Text('Receipt here..'),
-          )
-        ],
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Theme.of(context).colorScheme.secondary),
+                borderRadius: BorderRadius.circular(8)
+              ),
+              padding: const EdgeInsets.all(25),
+              child: Consumer<Restaurant>(
+                builder: (
+                  BuildContext context, 
+                  Restaurant restaurant, 
+                  Widget? child) 
+                  => Text(restaurant.displayCartReceipt())                
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
