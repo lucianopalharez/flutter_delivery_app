53dda71ae79225a8f251ff9e2bb54c44dc85fccb
L Palharês Rosa <lucianopalharesrosa@hotmail.com>
2024-08-22 12:01:12 -0300

    instala firebase_auth e instancia na classe authservice

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class AuthService {
   
   // chama o firebase auth
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   // pega o usuario atual
 
