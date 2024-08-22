7ec5cfb4f8b57786d66e0ff880bf833df5382b8c
L Palharês Rosa <lucianopalharesrosa@hotmail.com>
2024-08-22 12:21:11 -0300

    StreamBuilder reage ao estado de estar logado ou não

--- lib/services/auth/auth_middleware.dart
+++ lib/services/auth/auth_middleware.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_food/pages/home_page.dart';
+import 'package:flutter_food/services/auth/login_or_register.dart';
 
 class AuthMiddleware extends StatelessWidget {
   const AuthMiddleware({super.key});
@@ -12,9 +14,15 @@ class AuthMiddleware extends StatelessWidget {
         builder: (context, data) {
           
           // tem dados retornados
+          if (data.hasData) {
+            return const HomePage();
+          }
 
           // não tem dados retornados
-          
+          else {
+            return const LoginOrRegister();
+          }
+
         },
       ),
     );
